9211a0afd30ce21bf8a9a95ec4bfdfad62e8a9c0
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2024-03-28 21:03:25 +0500

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,17 +7,17 @@ const Product = (currentElem) => {
     <>
    
     <NavLink to={`/singleproduct/${id}`}>
-      <div className="card product-container w-[330px] h-[300px] rounded-[10px] border-top-0 border-1  hover:shadow-2xl hover:shadow-slate-600/50 mb-20">
-        <div className="rounded-md">
-          <img className="rounded-t-md " src={image} alt={name} />
+      <div className="card product-container w-[330px] h-[280px] border-top-0 border-1  hover:shadow-2xl hover:shadow-slate-600/50 mb-20">
+        <div className="">
+          <img className="w-100 " src={image} alt={name} />
+          <span className='bg-gray-100 font-bold text-black text-md rounded  opacity-55 hover:opacity-5  px-5 py-3 absolute top-2 left-[140px]'
+          >{category}
+          </span>
         </div>
-        <div className="card-data flex justify-center ">
-          <div className=" ">
-            <h2 className="text-2xl">{category}</h2>
-            <h3 className="text-md">{name}</h3>
+        <div className="card-data flex justify-between mx-5 my-2 ">
+            <h3 className="text-md font-bold">{name}</h3>
             <p className="text-md">{<FormatPrice price = {price}/>}</p>
           </div>
-        </div>
       </div>
     </NavLink>
    
