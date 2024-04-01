c3e5b22abef33ba9242ba51ac70b08928f7790e6
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2024-04-01 15:28:59 +0500

    formValidation done

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,6 +5,82 @@ import { Link, NavLink } from "react-router-dom";
 const Checkout = () => {
 
   const [items, setItems] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    address: '',
+    city: '',
+    zipCode: '',
+    mobile: '',
+    cashOnDelivery :false,
+  });
+  const [errors, setErrors] = useState({});
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setFormData({
+      ...formData,
+      [name]: newValue,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Check if any field is empty
+  const emptyFields = Object.entries(formData).filter(([key, value]) => {
+    return value === '' && key !== 'cashOnDelivery'; // Exclude checkbox from empty check
+  });
+
+    // Perform validation
+    const newErrors = {};
+    if (!formData.name) {
+      newErrors.name = 'Name is required';
+    }
+    if (!formData.email) {
+      newErrors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = 'Email is invalid';
+    }
+    if (!formData.address) {
+      newErrors.address = 'Address is required';
+    }
+    if (!formData.city) {
+      newErrors.city = 'City is required';
+    }
+    if (!formData.zipCode) {
+      newErrors.zipCode = 'Zip Code is required';
+    }
+    if (!formData.mobile) {
+      newErrors.mobile = 'Mobile is required';
+    }
+    if (!formData.cashOnDelivery) {
+      newErrors.cashOnDelivery = 'required';
+    }
+    setErrors(newErrors);
+
+    
+  // If there are no errors, place the order
+  if (Object.keys(newErrors).length === 0) {
+    setSuccessMessage("Order placed successfully!");
+    // Reset form data and clear success message after 2 seconds
+    setTimeout(() => {
+      setFormData({
+        name: "",
+        email: "",
+        address: "",
+        city: "",
+        zipCode: "",
+        mobile: "",
+        cashOnDelivery: false,
+      });
+      setErrors({});
+      setSuccessMessage("");
+    }, 2000);
+  }
+};
+
 	useEffect(() => {
 		fetch("http://localhost:3000/cart?_embed=product")
 		.then((response) => response.json())
@@ -52,7 +128,7 @@ const Checkout = () => {
       <div className=' bg-white pb-10 pt-40'>
         <div className='flex lg:flex-row flex-col items-start'>
       <div className="mx-auto w-[30%] mt-40 max-w-[700px]">
-      <form action="https://formspree.io/f/mleqwjwb" method="POST">
+      <form onSubmit={handleSubmit } method="POST">
       <div className="mb-5">
         <label
           for="name"
@@ -65,6 +141,7 @@ const Checkout = () => {
           name="name"
           id="name"
           placeholder="Full Name"
+          onChange={handleChange}
           required
           className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
@@ -81,6 +158,7 @@ const Checkout = () => {
           name="email"
           id="email"
           placeholder="example@domain.com"
+          onChange={handleChange}
           required
           className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
@@ -97,6 +175,7 @@ const Checkout = () => {
           name="address"
           id="address"
           placeholder="Address"
+          onChange={handleChange}
           required
           className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
@@ -113,6 +192,7 @@ const Checkout = () => {
           name="city"
           id="city"
           placeholder="City/Town"
+          onChange={handleChange}
           required
           className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
@@ -129,6 +209,7 @@ const Checkout = () => {
           name="zipCode"
           id="zipCode"
           placeholder="ZipCode"
+          onChange={handleChange}
           required
           className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
@@ -141,18 +222,25 @@ const Checkout = () => {
           Mobile*
         </label>
         <input
-          type="Number"
+          type="tel"
           name="mobile"
           id="mobile"
           placeholder="Mobile"
+          
+          onChange={handleChange}
           required
           className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
       </div>
      
       <div className="mb-5">
-      <input className='mr-4' type="checkbox" name="cashOnDelivery" id="cashOnDelivery" />
-      <label className="mb-3 text-md font-medium text-[#07074D]"  for="cashOnDelivery">Cash On Delivery</label>
+      <input className='mr-4'
+        type="checkbox"
+        name="cashOnDelivery" 
+        id="cashOnDelivery"
+        onChange={handleChange}
+        required />
+      <label className="mb-3 text-md font-medium text-[#07074D]"  for="cashOnDelivery">Cash On Delivery*</label>
       </div>
       <div className="mb-5">
         <label
@@ -169,7 +257,13 @@ const Checkout = () => {
           className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         ></textarea>
       </div>
-      
+      {Object.keys(errors).length > 0 && (
+        <p className="text-red-500 text-center">Please fill in all required fields.</p>
+      )}
+      {successMessage &&<p className="text-green-500">{successMessage}</p>}
+      <div className="mb-5">
+        <input type="submit" value="Submit" class=" mr-0 mt-6 w-full rounded-md bg-[#ffb524] py-2 font-medium text-blue-50 hover:bg-slate-500"/>
+      </div>
       <div>
       </div>
     </form>
@@ -225,10 +319,8 @@ const Checkout = () => {
           <p class="text-lg font-bold">Total:</p>
           <div class="">
             <p class="mb-1 text-lg font-bold text-end">${subTotal + shippingCharges}</p>
-         
-          <button onClick={() => alert("please fill form properly")} class=" mr-0 mt-6 w-full rounded-md bg-[#ffb524] py-1.5 font-medium text-blue-50 hover:bg-slate-500">Place Order</button>
           <NavLink to={'/store'}>
-          <button class="mt-6 w-full rounded-md bg-[#81c408] py-1.5 font-medium text-blue-50 hover:bg-slate-500"> Continue Shopping </button>
+          <button class="mt-6 w-full rounded-md bg-[#81c408] py-2 px-10 text-nowrap font-medium text-blue-50 hover:bg-slate-500"> Continue Shopping </button>
           </NavLink> 
     </div>
   </div>
