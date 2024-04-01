e5946a2838a2863db3e63e3deb68299e74107e00
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2024-04-01 12:22:31 +0500

    checkOut page layout is set

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -51,7 +51,7 @@ const Checkout = () => {
     <>
       <div className=' bg-white pb-10 pt-40'>
         <div className='flex lg:flex-row flex-col items-start'>
-      <div className="mx-auto w-full mt-40 max-w-[550px]">
+      <div className="mx-auto w-[30%] mt-40 max-w-[700px]">
       <form action="https://formspree.io/f/mleqwjwb" method="POST">
       <div className="mb-5">
         <label
@@ -175,9 +175,9 @@ const Checkout = () => {
     </form>
   </div>
   {/* cart data */}
-  <div className=" py-40  flex flex-col md:flex-row gap-10 md:items-start md:px-20">
-      <div className='w-full '>
-        <table className='w-[100%]'>
+  <div className="w-[60%]  py-40 gap-10 flex flex-col md:flex-row md:items-start md:px-10 ">
+      <div className=''>
+        <table className='w-full'>
           <thead className='text-sm'>
           <tr className='border-b border-black'>
             <th className='py-5 text-left '>Products</th>
@@ -196,7 +196,7 @@ const Checkout = () => {
                   <img className='w-12 h-12 rounded-full' src={"../images/" + item.product.image} alt={item.product.name} />
                 </td>
                 <td className="text-center ">{item.product.name}</td>
-                <td className="text-center ">{'$' + item.product.price} / {item.product.unit}</td>
+                <td className="text-center px-1">{'$' + item.product.price} / {item.product.unit}</td>
                 <td className="text-center ">{item.quantity}</td>
                 <td className="text-center ">{'$' + item.product.price * item.quantity}</td>
                 <td className='text-2xl text-red-500'>
@@ -208,7 +208,7 @@ const Checkout = () => {
         </table>
       </div>
       {/* Sub total */}
-      <div class=" h-full rounded-lg border bg-slate-100 shadow-md md:mt-0 md:w-1/3 p-6">
+      <div class=" rounded-lg border bg-slate-100 shadow-md md:mt-0 md:w-1/3 p-6 ">
         <div className='text-center py-2'>
           <h2 className="text-4xl font-medium text-[#45595b]">Sub Total</h2>
         </div>
@@ -221,96 +221,15 @@ const Checkout = () => {
           <p class="text-gray-700">{'$' + shippingCharges}</p>
         </div>
         <hr class="my-4" />
-        <div class="flex justify-between">
-          <p class="text-lg font-bold">Total</p>
+        <div class="flex">
+          <p class="text-lg font-bold">Total:</p>
           <div class="">
-            <p class="mb-1 text-lg font-bold">${subTotal + shippingCharges}</p>
-            <p class="text-sm text-gray-700">including VAT</p>
-      {/* <div className=' bg-gray-100 px-40 py-10'>
-        <h2 className=" text-[#45595b] text-5xl font-semibold">Billing details</h2>
-        <div className='checkout_inner flex items-start'>
-          <div className=" form_container mx-auto w-full max-w-[550px] mt-[150px]">
-          <form action="https://formspree.io/f/mleqwjwb" method="POST">
-            <div className="mb-5">
-              <label
-                for="name"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
-                Full Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Full Name"
-                required
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
-            </div>
-            <div className="mb-5">
-              <label
-                for="email"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="example@domain.com"
-                required
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
-            </div>
-            <div className="mb-5">
-              <label
-                for="subject"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
-                Subject
-              </label>
-              <input
-                type="text"
-                name="subject"
-                id="subject"
-                placeholder="Enter your subject"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              />
-            </div>
-            <div className="mb-5">
-              <label
-                for="message"
-                className="mb-3 block text-base font-medium text-[#07074D]"
-              >
-                Message
-              </label>
-              <textarea
-                rows="4"
-                name="message"
-                id="message"
-                placeholder="Type your message"
-                required
-                className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-              ></textarea>
-            </div>
-            <div>
-              <button
-                className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-        </div>
-        <NavLink to={'/checkout'}>
-        <button class="mt-6 w-full rounded-md bg-[#ffb524] py-1.5 font-medium text-blue-50 hover:bg-slate-500" onClick={() => checkout()}> Place Order</button>
-        </NavLink>
-        <NavLink to={'/store'}>
-        <button class="mt-6 w-full rounded-md bg-[#81c408] py-1.5 font-medium text-blue-50 hover:bg-slate-500"> Continue Shopping </button>
-        </NavLink>
-      </div> */}
+            <p class="mb-1 text-lg font-bold text-end">${subTotal + shippingCharges}</p>
+         
+          <button onClick={() => alert("please fill form properly")} class=" mr-0 mt-6 w-full rounded-md bg-[#ffb524] py-1.5 font-medium text-blue-50 hover:bg-slate-500">Place Order</button>
+          <NavLink to={'/store'}>
+          <button class="mt-6 w-full rounded-md bg-[#81c408] py-1.5 font-medium text-blue-50 hover:bg-slate-500"> Continue Shopping </button>
+          </NavLink> 
     </div>
   </div>
   </div>
