6b43309069f063e3f8c974a432c5beab7a4ce066
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2024-04-17 17:05:45 +0500

    service section is modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,8 @@ import {Link, NavLink} from "react-router-dom";
 import { FaCarSide } from "react-icons/fa6";
 import { MdOutlineSecurity } from "react-icons/md";
 import { FaArrowRightArrowLeft } from "react-icons/fa6";
+import { MdOutlineRocketLaunch } from "react-icons/md";
+import { CiCreditCard1 } from "react-icons/ci";
 import { FaPhone } from "react-icons/fa6";
 import ProductListing from './ProductListing';
 import ServiceItems from './ServiceItems';
@@ -12,20 +14,20 @@ import Testimonials from './Testimonials';
 import Header from './Header';
 
 const featuresData =[
-  {icon:<FaCarSide className='text-5xl text-[#fff]'/>, name:'Free Shipping', description:'Free on order over $300'},
-  {icon:<MdOutlineSecurity className='text-5xl text-[#fff]'/>, name:'Security Payment', description:'100% security payment'},
-  {icon:<FaArrowRightArrowLeft className='text-5xl text-[#fff]'/>, name:'30 Day Return', description:'30 day money guarantee'},
-  {icon:<FaPhone className='text-5xl text-[#fff]'/>, name:'24/7 Support', description:'Support every time fast'},
+  {icon:<MdOutlineRocketLaunch className='text-5xl text-[#fff]'/>, name:'Home Delivery', description:'All over Lahore'},
+  {icon:<CiCreditCard1 className='text-5xl text-[#fff]'/>, name:'Cash on delivery', description:'Pay when you get it'},
+  // {icon:<FaArrowRightArrowLeft className='text-5xl text-[#fff]'/>, name:'30 Day Return', description:'30 day money guarantee'},
+  {icon:<FaPhone className='text-5xl text-[#fff]'/>, name:'Call Us', description:'03097949844'},
 ];
 
 const Home = () => {
   return (
     <>
        <Header />
-      {/* <div className='w-full flex flex-wrap justify-center items-center px-40 md:px-0 sm:px-0 py-5 '>
+      <div className='w-full flex flex-wrap justify-center items-center px-40 md:px-0 sm:px-0 py-5 '>
       {featuresData.map((item, index)=>(
-      <div key={index} className='w-[300px] h-[300px] flex justify-evenly items-center'>
-      <div className=" bg-[#f2f4f6] rounded p-10 flex relative">
+      <div key={index} className='w-[300px] h-[300px] mx-10'>
+      <div className=" bg-[#f2f4f6] rounded p-10 relative ">
       <div className=" w-[100px] h-[100px] bg-[#ffb524] py-5 rounded-[50%]  mt-[-40px] flex justify-center items-center absolute top-14 right-[100px]">
       {item.icon}
       </div>
@@ -36,11 +38,11 @@ const Home = () => {
       </div>
       </div>
       ))}
-      </div> */}
-      <ProductListing />
+      </div>
+      {/* <ProductListing /> */}
       {/*-------------- services section------------------ */}
        <ServiceItems /> 
-      <FreshOrganicVegetables />
+      {/* <FreshOrganicVegetables /> */}
       <Banner />
       <Testimonials /> 
     </>
