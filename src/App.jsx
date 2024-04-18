bb3407d963afc869e27e6df9b5f29f7c188ed32a
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2024-04-18 14:48:46 +0500

    click to chat on whatsapp feature is implemented

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { useState } from "react";
-
+import { ImWhatsapp } from "react-icons/im";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 const App = () => {
 	const [dataFromCart, setDataFromCart] = useState('');
@@ -16,8 +17,12 @@ const App = () => {
 		
 		<>
 		<div className="">
+			
 			<Navbar sendDataToApp = {receiveDataFromCart}/>
 			{/* <Header /> */}
+			<Link to="https://wa.me/03097949844">
+			<div className="w-20 h-20"><ImWhatsapp className=" w-16 h-16 fixed bottom-10 left-10 text-green-500 z-1"></ImWhatsapp></div>
+			</Link>
 			<Routing />
 			<Footer />
 			</div>	
